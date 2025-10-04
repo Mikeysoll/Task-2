e34e227c48543f49cbf0f2777c5433406a214dba
Mikhail Solovyov <solovev02@yandex.ru>
2025-10-04 19:14:54 +0300

    Update Main.java

--- src/main/java/ru/yandex/solovev02/Main.java
+++ src/main/java/ru/yandex/solovev02/Main.java
@@ -2,7 +2,7 @@ package ru.yandex.solovev02;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет, мир!");  // Выводит текст на экран
+        System.out.println("Привет, мир!!!");  // Выводит текст на экран
     }
 }
 
